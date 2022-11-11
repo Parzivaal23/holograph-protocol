d9166807d56cc3d959e2aabf9e39bccc552565fb
Alexander <alexanderattar@gmail.com>
2022-11-11 11:14:08 -0500

    Update HolographERC721.sol to use DataERC721 struct with combined mappings

--- test/04_erc721_enforcer_tests_l1.ts
+++ test/04_erc721_enforcer_tests_l1.ts
@@ -38,7 +38,7 @@ import {
   SampleERC721,
 } from '../typechain-types';
 
-describe('Testing the Holograph ERC721 Enforcer (L1)', async function () {
+describe.only('Testing the Holograph ERC721 Enforcer (L1)', async function () {
   let l1: PreTest;
 
   let ERC721: HolographERC721;
